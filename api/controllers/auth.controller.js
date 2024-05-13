28399d66599d6e0c749807520a3c40217fabe098
Venya <venyadurgam25@gmail.com>
2024-05-12 23:26:29 -0500

    signup api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import User from '../models/user.model.js';
-
+import bcryptjs from 'bcryptjs'
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -9,6 +9,6 @@ export const signup = async (req, res, next) => {
     await newUser.save();
     res.status(201).json('User created successfully!');
   } catch (error) {
-    next(error);
+    res.status(500).josn(error.message)
   }
 };
